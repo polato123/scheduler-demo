fdd1b84e3f56fa8df6638c896ca1d9763b8b10e8
Bruno <bruno_polato@hotmail.com>
2017-12-02 12:30:16 -0200

    feito

--- src/main.c
+++ src/main.c
@@ -4,11 +4,36 @@ int main() {
   int a[5];
   int n;
 
+
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
-  for (int i=0; i<n; i++) {
-    /* Imprime resultado da n-esima iteracao do scheduler */
+  for (int i=1; i<n+1; i++) {
+ 	
+	if(i%a[0]==0)   //se a iteracao atual i for divisivel por a[0]
+	printf("0");	
+	if(i%(a[1])==0)  //se a ...                              a[1]   
+	printf("1");
+	if(i%(a[2])==0)    //se a ...                              a[2], e por ai vai
+	printf("2");
+	if(i%(a[3])==0)
+	printf("3");
+	if(i%(a[4])==0)
+	printf("4");
+  	
+	if((i%(a[0])!=0)&&(i%(a[1])!=0)&&(i%(a[2])!=0)&&(i%(a[3])!=0)&&(i%(a[4])!=0))  
+        //caso nao haja nenhum contador de periodicidade divisivel
+	
+        printf("-");
+        printf("\n");
+        
+	//if(i==n)
+	//printf("\n");
+	
+      
+
   }
+
+  
 
   return 0;
 }
